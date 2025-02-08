b9cfa9fa5f386e4a370acdb2ccb2ef39110ae1af
sera0130 <sera.fav.post@gmail.com>
2025-02-08 18:43:56 +0900

    初期表示エラーレスポンス表示

--- src/app/views/pages/page1/page1.component.ts
+++ src/app/views/pages/page1/page1.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { environment } from '../../../../environments/environment';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CustomValidator } from '../../../common/validators/numeric.validator';
@@ -12,11 +12,12 @@ import { CommonModule } from '@angular/common';
   styleUrl: './page1.component.scss',
   standalone: true,
 })
-export class Page1Component {
+export class Page1Component implements OnInit {
   form: FormGroup;
   requiredError = false;
   numericError = false;
-  responseError = false;
+  onInitResponseError = false;
+  submitResponseError = false;
 
   constructor(private fb: FormBuilder, private httpService: HttpService) {
     this.form = this.fb.group({
@@ -24,12 +25,22 @@ export class Page1Component {
     });
   }
 
+  ngOnInit(): void {
+    // APIコール
+    this.httpService.post('your-endpoint', this.form.value).subscribe(response => {
+      console.log('API call successful', response);
+    }, error => {
+      this.onInitResponseError = true;
+      console.error('API call failed', error);
+    });
+  }
+
   /**
    * フォームの送信処理
    */
-  onSubmit() {
+  onSubmit(): void {
     // 初期化
-    this.responseError = false;
+    this.submitResponseError = false;
 
     // 入力値チェック
     this.validateForm();
@@ -39,7 +50,7 @@ export class Page1Component {
       this.httpService.post('your-endpoint', this.form.value).subscribe(response => {
         console.log('API call successful', response);
       }, error => {
-        this.responseError = true;
+        this.submitResponseError = true;
         console.error('API call failed', error);
       });
     } else {
@@ -50,7 +61,7 @@ export class Page1Component {
   /**
    * フォームのバリデーション処理
    */
-  validateForm() {
+  validateForm(): void {
     this.requiredError = this.form.get('inputOneTime')?.errors?.['required'] || false;
     this.numericError = this.form.get('inputOneTime')?.errors?.['numeric'] || false;
   }
