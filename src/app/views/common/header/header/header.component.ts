01cfabd32002099a70659c7931910e255306121e
sera0130 <sera.fav.post@gmail.com>
2025-02-09 15:54:07 +0900

    ヘッダー調整

--- src/app/views/common/header/header/header.component.ts
+++ src/app/views/common/header/header/header.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
